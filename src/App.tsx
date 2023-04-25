e7cf11992d9de18c7ef94ee4ce7e53143c16c6c3
CesarCachay <cesarcachay1996@gmail.com>
2023-04-24 17:57:42 -0700

    Add router, global styles and theme on the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-function App() {
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/layout';
+import theme from './utils/theme';
+
+const App: React.FC = () => {
+  const globalStyles = {
+    display: 'block',
+    margin: '0',
+    padding: '0',
+    width: '100%',
+    heigth: '100%',
+    fontFamily: 'Open-Sans, Helvetica, Sans-Serif'
+  };
+
+  const GlobalStyle = createGlobalStyle`
+    body {
+      ${globalStyles}
+    }
+  `
+
   return (
-    <div>
-      Start of project
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </React.Fragment>
   )
 }
 
---
+++ src/utils/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    textTitle: '#323754',
+    textPrimary: '#404071',
+    textSecondary: '#72758c',
+    disabled: '#8C8CB1',
+    placeholderColor: '#C8C8D3',
+    borderColor: '#EFEFF5',
+    bgColor: '#fffffff',
+    darkBgColor: 'black',
+    success: '#79E29E',
+    error: '#F1564F',
+    errorBg: '#ffd9d9',
+    redHover: '#ee3830',
+  },
+};
+
+export default theme;
